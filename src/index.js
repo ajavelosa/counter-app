e77bed57a31566982153f39a8d6996a60b94c10c
Anton Javelosa <anton.javelosa@uber.com>
2022-11-28 13:21:17 +0800

    removing unnecessary text from index

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import Counter from './components/counter';
 
-const root = ReactDOM.createRoot(document.getElementById('root')).render(<Counter />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Counter />)
 reportWebVitals();

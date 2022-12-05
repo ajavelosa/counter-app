ed325e0f1a4f58b21ed6c365681b1bf223cf8b21
Anton Javelosa <anton.javelosa@uber.com>
2022-12-05 09:43:10 +0800

    added lifecycle hooks

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  componentDidUpdate(prevProps, prevState) {
+    console.log("prevProps", prevProps);
+    console.log("prevState", prevState);
+  }
+
+  componentWillUnmount() {
+    console.log("Counter - Unmount");
+  }
+
   render() {
     const { onIncrement, onDelete, counter, children } = this.props;
     console.log("Counter - Rendered");
